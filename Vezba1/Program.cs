36ae6ef2348685b8e6520006e270a9abccb8ca6d
jorgoslambrinidis <jorgos.lambrinidis@gmail.com>
2023-03-29 20:06:58 +0200

    second commit

---
+++ Vezba1/Program.cs
@@ -0,0 +1,101 @@
+﻿using Vezba1;
+
+Console.WriteLine("Hello, World!!!");
+// Console.ReadKey();
+
+/* ova 
+ * e
+ * multiline
+ * comment
+  */
+
+// ova e single line comment
+
+//string a = "nekoja recenica";
+//int b = 30;
+//double c = 0.2;
+
+//int a = 1;
+//int b = 2;
+//int c = 3;
+//int d = a /* ova e objasnuvanje za promenliva a */ + c;
+
+//0  //1  //2 //3  //4
+//Console.WriteLine("Rezultatot na 1 + 3 = {4}", "SMX", 1,   2,  3, 4);
+//Console.WriteLine(36 - 6);
+//Console.WriteLine(36 / 6);
+//Console.WriteLine(5 * 10);
+
+//string text = Console.ReadLine();
+//Console.WriteLine("Ova e tekstot koj sto e vnesen od strana na user: {0}", text);
+
+Console.WriteLine("Vnesete gi posledovaltelno 4te varijabli a, b, c, d");
+Console.WriteLine("******************************************************");
+Console.WriteLine("Vnesete go prviot broj: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+Console.WriteLine("Vnesete go vtoriot broj: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b == 2)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+Console.WriteLine("Vnesete go tretiot broj: ");
+int c = int.Parse(Console.ReadLine());
+
+if (c == 3)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+Console.WriteLine("Vnesete go cetvrtiot broj: ");
+int d = int.Parse(Console.ReadLine());
+
+if (d == 4)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
+if (a == 1 && b == 2 && c == 3 && d == 4)
+{
+    int result = a + b + c + d;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+
+
+
+
+
+//string space = "/";
+// "1234"
+
+
