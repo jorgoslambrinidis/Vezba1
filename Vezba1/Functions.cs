2dc1d88fcb1dd54295240cda7c0496768bc772df
jorgoslambrinidis <jorgos.lambrinidis@gmail.com>
2023-03-31 20:35:37 +0200

    voved vo c# function

---
+++ Vezba1/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezba1
+{
+    public class Functions
+    {                                         // "1" ili "text1"
+        public bool IsNumber(string imeNaVlezenParametarString)
+        {
+            // mesto za logika da proveri dali vlezniot parametar e number ili ne e
+
+
+
+
+            return true;
+        }
+    }
+}
--- Vezba1/Program.cs
+++ Vezba1/Program.cs
@@ -15,13 +15,16 @@ Console.WriteLine("Hello, World!!!");
 //int b = 30;
 //double c = 0.2;
 
-//int a = 1;
-//int b = 2;
-//int c = 3;
-//int d = a /* ova e objasnuvanje za promenliva a */ + c;
+int a = 1;
+int b = 2;
+int c = 3;
+int d = a /* ova e objasnuvanje za promenliva a */ + c;
+int finalResult = 4;
 
 //0  //1  //2 //3  //4
-//Console.WriteLine("Rezultatot na 1 + 3 = {4}", "SMX", 1,   2,  3, 4);
+Console.WriteLine($"Rezultatot na 1 + 3 = {finalResult}");
+
+
 //Console.WriteLine(36 - 6);
 //Console.WriteLine(36 / 6);
 //Console.WriteLine(5 * 10);
@@ -29,66 +32,68 @@ Console.WriteLine("Hello, World!!!");
 //string text = Console.ReadLine();
 //Console.WriteLine("Ova e tekstot koj sto e vnesen od strana na user: {0}", text);
 
-Console.WriteLine("Vnesete gi posledovaltelno 4te varijabli a, b, c, d");
-Console.WriteLine("******************************************************");
-Console.WriteLine("Vnesete go prviot broj: ");
-int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Vnesete gi posledovaltelno 4te varijabli a, b, c, d");
+//Console.WriteLine("******************************************************");
+//Console.WriteLine("Vnesete go prviot broj: ");
+//int a = int.Parse(Console.ReadLine());
 
-if (a == 1)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+//int imeNaPrvaPromenliva = 0;
 
-Console.WriteLine("Vnesete go vtoriot broj: ");
-int b = int.Parse(Console.ReadLine());
+//if (a == 1)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
 
-if (b == 2)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+//Console.WriteLine("Vnesete go vtoriot broj: ");
+//int b = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Vnesete go tretiot broj: ");
-int c = int.Parse(Console.ReadLine());
+//if (b == 2)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
 
-if (c == 3)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+//Console.WriteLine("Vnesete go tretiot broj: ");
+//int c = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Vnesete go cetvrtiot broj: ");
-int d = int.Parse(Console.ReadLine());
+//if (c == 3)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
 
-if (d == 4)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+//Console.WriteLine("Vnesete go cetvrtiot broj: ");
+//int d = int.Parse(Console.ReadLine());
+
+//if (d == 4)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
 
 
-if (a == 1 && b == 2 && c == 3 && d == 4)
-{
-    int result = a + b + c + d;
-    Console.WriteLine(result);
-}
-else
-{
-    Console.WriteLine("Error");
-}
+//if (a == 1 && b == 2 && c == 3 && d == 4)
+//{
+//    int result = a + b + c + d;
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("Error");
+//}
 
 
 
